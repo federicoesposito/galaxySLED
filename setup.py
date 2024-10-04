8691585d52b0f832e340e735df1e2c5f125f19f0
Federico Esposito <federico.esposito7@unibo.it>
2024-10-04 02:09:45 +0200

    galaxySLED 0.1.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 # Setup
 setup(
     name="galaxysled",
-    version="0.1.4",
+    version="0.1.6",
     description="A code to reproduce and fit a galaxy CO SLED",
     long_description=long_description,
     long_description_content_type="text/markdown",
